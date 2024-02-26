e157d83ac4f57253bc79369221ce3e2622e53a92
Lukáš Skřivánek <luky.skrivos@gmail.com>
2024-02-26 09:19:12 +0100

    Initial Commit

---
+++ Coffe/CoffeApp.swift
@@ -0,0 +1,17 @@
+//
+//  CoffeApp.swift
+//  Coffe
+//
+//  Created by macbook on 26.02.2024.
+//
+
+import SwiftUI
+
+@main
+struct CoffeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
